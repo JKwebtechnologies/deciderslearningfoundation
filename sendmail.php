33a732e5c9cdb3d6ac6ecf3e741600e06c1af103
JKwebtechnologies <jamiemartin09@gmail.com>
2025-11-18 23:09:06 +0530

    form fixed

--- sendmail.php
+++ sendmail.php
@@ -1,26 +1,26 @@
 <?php
-
-if($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $name    = htmlspecialchars($_POST['name']);
     $email   = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    // Change this to your email address
-    $to = "santhosh5198kumar@gmail.com";
+    // Send to two email addresses
+    $to = "santhosh5198kumar@gmail.com, ramadurai@deciderslearningfoundation.org, maran@deciderslearningfoundation.org, jamiemartin09@gmail.com";
+
     $subject = "New Contact Form Message";
 
-    $body = "
-    Name: $name
-    Email: $email
-    Message:
-    $message
-    ";
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
 
-    $headers = "From: $name <$email>\r\n";
+    // Must be domain email
+    $from = "no-reply@" . $_SERVER['SERVER_NAME'];
+
+    $headers  = "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+    $headers .= "From: $from\r\n";
     $headers .= "Reply-To: $email\r\n";
 
-    if(mail($to, $subject, $body, $headers)) {
+    if (mail($to, $subject, $body, $headers)) {
         echo "Message sent successfully!";
     } else {
         echo "Message sending failed.";
